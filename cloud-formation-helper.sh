55330a9a9c5522ec5811325f6306d9530e467fa5
Giovanni Baratta <bargiovi@hotmail.it>
2020-05-01 10:54:30 +0200

    vpc, 2 pub net, 2 priv net, autoscaling group

---
+++ cloud-formation-helper.sh
@@ -0,0 +1,19 @@
+# $1 : action to execute, value: [create, update]
+# $2 : stack name
+# $3 : template body file name
+# $4 : load par, if present load parameters from $3.json
+ 
+PARAMS=""
+
+if [[ $# -eq 4 ]]
+then
+    #remove extension
+    FILE_NAME=$(echo $3 | awk '{split($0, output, ".yaml"); print output[1]}' )
+    PARAMS="--parameters file://$FILE_NAME.json"
+fi
+
+aws cloudformation "$1"-stack \
+        --region us-east-1 \
+        --stack-name $2 \
+        --template-body file://$3 \
+        $PARAMS
---
+++ createStack.sh
@@ -0,0 +1,3 @@
+chmod u+x cloud-formation-helper.sh
+
+./cloud-formation-helper.sh create "$@"
